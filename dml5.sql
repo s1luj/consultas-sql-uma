1e0dc80a43533c6d2ac8c8d7a008633bf8ba89d7
s1luj <joseluisfernandezmarquez@gmail.com>
2018-01-17 15:31:15 +0100

    17-01-2018

--- dml5.sql
+++ dml5.sql
@@ -43,3 +43,60 @@ from asignaturas2;
 -- falta el delete
 
 rollback;
+
+
+
+-- VISTAS -- es un select con un nombre
+
+select * from alumnos;
+create table alumnos2 as select * from alumnos;
+
+create or replace view Alumnos_con_experiencia(Codigo,Cuando) as (select Dni, to_char(fecha_prim_matricula,'yyyy') from alumnos2 where apellido1 like 'M%');
+
+select *
+from Alumnos_con_experiencia
+WHERE to_number(to_char(sysdate,'yyyy')) - to_number(Cuando) > 2;
+
+
+-- OPCIONES DE VISTA
+create or replace view al2 as select * from alumnos where apellido1 like 'M%' With check option; --PONIENDO WITH CHECK OPTION SOLO SE PUEDEN AÑADIR LUEGO CON INSERT LA GENTE CON APELLIDOS QUE EMPIECEN POR M
+
+select * from al2;
+
+desc alumnos;
+
+insert into al2 (dni,nombre,apellido1,fecha_prim_matricula)
+values (4444, 'GANDALF', 'MARTIN', sysdate);
+
+-- LA PARTE QUE VIENE AHORA DE PERMISOS SOLO CAEN EN EL TEST DE TEORIA
+
+--grant y revoke... 
+
+-- user = todo lo mío ( de mi usuario)
+SELECT * from user_tables; -- a traves de esta tabla vemos una lista de todas las tablas propias de 'mi usuario'
+select * from user_views; -- a traves de esta tabla vemos una lista de todas las vistas propias de 'mi usuario'
+select * from user_tab_columns where table_name = 'ALUMNOS2'; -- (cuidado que todas la tablas y demas se guarda en mayusculas(el nombre)) (si hubiesemos puesto 'alumnos2' en minusculas no sale nada <- puede caer en test
+select * from user_constraints; -- R=clave foranea 
+-- all = todo a lo que tenemos acceso
+select * from all_tables order by table_name; -- tabla alumnos -> el usuario docencia hizo un grant para que podamos ver la tabla alumnos, impartir, etc..
+select * from all_views;
+
+select * from user_constraints;
+
+select user from dual; -- mi usuario
+
+select *
+from all_tables
+where owner != user; -- != es igual que <> (... es distinto de...)
+
+
+
+-- TRIGGERS (esto solo cae en el test creo) (las diapositivas estan en la sala comun>tema1>sql>disparadores y trabajos.pdf)
+
+create or replace trigger control_alumnos2
+after insert or delete or update on alumnos2
+begin
+    insert into ctrl_alumnos2(Tabla,Usuario,Fecha)
+    values ('Alumnos2', user,sysdate);
+end control_alumnos2;
+
